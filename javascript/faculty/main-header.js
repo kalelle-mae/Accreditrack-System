593cd28cf341f4fb8d10c9984bc0ff6f5cb2adb1
kalelle-mae <kalellemaevictorio@gmail.com>
2025-12-24 18:22:21 +0800

    updates main-header.js and top-section.js

--- javascript/faculty/main-header.js
+++ javascript/faculty/main-header.js
@@ -1,4 +1,4 @@
-fetch('')
+fetch('/html/role/faculty/top-section.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('main-header-placeholder').innerHTML = data;
