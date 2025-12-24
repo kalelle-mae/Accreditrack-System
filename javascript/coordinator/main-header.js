98e3ef970b02b46f2e2084c82179d14246fc57ee
kalelle-mae <kalellemaevictorio@gmail.com>
2025-12-24 12:34:36 +0800

    update top section in coordinator side

---
+++ javascript/coordinator/main-header.js
@@ -0,0 +1,6 @@
+fetch('/html/role/coordinator/top-section.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('main-header-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
